61bf9c8a6d710b943178a1766bb67b82121579d3
osama seid <osaseid1@gmail.com>
2025-08-29 15:23:06 +0300

    updated

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,9 +1,10 @@
+import { response } from 'express';
 import Notes from '../models/notes.js';
 
 
 export async function getAllNotes(req, res){
     try{
-        const notes = await Notes.findx();
+        const notes = await Notes.find();
             res.status(200).json(notes);
 
     }catch(error){
@@ -12,15 +13,45 @@ export async function getAllNotes(req, res){
     }    
 }
 
-export function createNote(req, res){
-    res.status(201).json({message:"note created"});
+export async function createNote(req, res){
+    try{
+        const {title, content} = req.body;
+        if(!title || !content){
+            return res.status(400).json({message: "Title and content are required"});
+        }
+        const newNote = new Notes({title, content});
+        await newNote.save();
+
+    }
+    catch(error){
+        res.status(500).json({message: "Internal server error"})
+        console.log("error in createNote:", error.message);
+    }
 }
 
-export function deleteNote(req, res){
-    res.status(200).json({message:"note deleted"});
+export async function deleteNote(req, res){
+    try{
+        const deleteNote= await Notes.findByIdAndDelete(req.params.id);
+        if(!deleteNote) return response.status(404).json({message: "Note not found"});
+    }
+    catch(error){
+        res.status(500).json({message: "Internal server error"})
+        console.log("error in deleteNote:", error.message);
+        res.status(200).json({message: "Note deleted successfully"});           
+    }
+
 }
 
-export function updateNote(req, res){
-    res.status(200).json({message: "note updated"});
+export async function updateNote(req, res){
+    try{
+        const {title, content}=req.body;
+        const updateNote= await Notes.findByIdAndUpdate(req.params.id,{title, content});
+        if(!updateNote) return response.status(404).json({message: "Note not found"});
+    }
+    catch(error){
+        res.status(500).json({message: "Internal server error"})
+        console.log("error in updateNote:", error.message);
+        res.status(200).json({message: "Note updated successfully"});           
+    }
+   
 }
-
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import notesRoutes from './route/notesRoutes.js';
 import {connectdb} from './config/db.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 connectdb();
 
+
 const port = process.env.PORT || 5001;
 const app = express();
+
+//middlewares
+app.use(express.json());
 
 app.use('/api/notes', notesRoutes);
 
